949be47f38463b55376cedd47b416594a8cc21b5
crossopt <s17b2_fedorkina@179.ru>
2020-04-22 17:10:55 +0300

    something something UI

--- src/main/java/fr/ladybug/team/MyClientUI.java
+++ src/main/java/fr/ladybug/team/MyClientUI.java
@@ -2,10 +2,28 @@ package fr.ladybug.team;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.swing.filechooser.FileView;
+
 public class MyClientUI extends Application {
+    private static final int BASE_SCREEN_WIDTH = 200;
+    private static final int BASE_SCREEN_HEIGHT = 200;
+    private static final int MIN_SCREEN_HEIGHT = 200;
+    private static final int MIN_SCREEN_WIDTH = 100;
+
     private MyClient client;
 
     public static void main(String[] args) {
@@ -14,15 +32,35 @@ public class MyClientUI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        TextInputDialog remoteAddressSupplier = new TextInputDialog("ip.ad.dr.re:port");
-        remoteAddressSupplier.setTitle("MyFTP");
-        remoteAddressSupplier.setHeaderText("Welcome to MyFTP");
-        remoteAddressSupplier.setContentText("Enter message");
-        var result = remoteAddressSupplier.showAndWait();
+        var dataSupplier = FXCollections.observableArrayList();
+        var listView = new ListView<>(dataSupplier);
 
-        if (!result.isPresent()) {
-            Platform.exit();
-            return;
-        }
+        final TextField comment = new TextField();
+        comment.setPromptText("Enter your comment.");
+        comment.setOnAction(actionEvent -> {
+            System.out.println(comment.getText());
+            dataSupplier.addAll(comment.getText());
+            comment.setText("");
+        });
+
+        GridPane pane = new GridPane();
+        GridPane.setConstraints(comment, 0, 0);
+        GridPane.setConstraints(listView, 0, 1);
+        pane.setAlignment(Pos.CENTER);
+
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setHgrow(Priority.ALWAYS);
+        pane.getColumnConstraints().add(cc);
+
+        pane.getChildren().add(comment);
+        pane.getChildren().add(listView);
+
+        Scene scene = new Scene(pane, BASE_SCREEN_WIDTH, BASE_SCREEN_HEIGHT);
+        primaryStage.setTitle("Messenger");
+        primaryStage.setScene(scene);
+        primaryStage.setMinHeight(MIN_SCREEN_HEIGHT);
+        primaryStage.setMinWidth(MIN_SCREEN_WIDTH);
+        primaryStage.show();
+
     }
 }
